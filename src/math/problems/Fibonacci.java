59fd43550a9234de355ab6adab2092edda28886f
Mohammed Mehadi <mohammedmehadi2@gmail.com>
2018-12-23 13:07:00 -0500

    Fibonacci is done

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,11 +1,24 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
          */
+        int [] q = new int[40];
 
+        for (int i = 0;i <40; i ++){
+
+            if (i <2){
+
+                q[i]=i;}
+            else {
+                q[i]=q[i-2] + q [i -1] ;
+            }
+        }
+        System.out.println("40 Fibonacci numbers : "+Arrays.toString(q));
 
     }
 }
