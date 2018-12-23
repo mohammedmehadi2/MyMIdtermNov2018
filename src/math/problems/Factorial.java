e48cdbeb1893a7621d4c2588d7fbaa4324cb8fdf
Mohammed Mehadi <mohammedmehadi2@gmail.com>
2018-12-23 12:48:26 -0500

    Factorial is done

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,26 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        // Using Iteration
+        int q = 5;
+        int outcome = 1;
+        for (int i = 1; i <= q; i++) {
+
+            outcome = outcome * i;
+
+        }
+        System.out.println("The factorial of 5(By using iteration) : " +outcome);
+        System.out.println("The factorial of 5(By using recursion) : " + recurs(5));
+
+    }public static int recurs(int q){
+
+        if (q==1){
+
+            return 1;}
+        else {
+
+            return q*recurs(q-1);}
 
     }
+
 }
